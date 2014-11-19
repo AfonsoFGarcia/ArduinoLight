3183744de5a23a392298fb2cad48d91157ff7ff6
Afonso F. Garcia <afonsogarcia@icloud.com>
2014-11-19 12:27:18 +0200

    Fist version of the light server

---
+++ light_server.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+from thread import *
+
+if len(sys.argv) < 3:
+	print 'Usage python server.py [ARDUINO IP] [LIGHT THRESHOLD]'
+	sys.exit(-1)
+
+MY_TCP_IP = ''
+MY_TCP_PORT = 5000
+BUFFER_SIZE = 3
+
+ARD_TCP_IP = sys.argv[1]
+ARD_TCP_PORT = 5001
+
+LIGHT_ON = 'L'
+THRS = int(sys.argv[2])
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((MY_TCP_IP, MY_TCP_PORT))
+s.listen(1)
+
+def num(s):
+	try:
+		return int(s)
+	except ValueError:
+		return float(s)
+
+def getlight():
+	global ARD_TCP_IP
+	global ARD_TCP_PORT
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((ARD_TCP_IP, ARD_TCP_PORT))
+	s.send('REQ')
+	data = s.recv(10)
+	s.close()
+	return num(data)
+
+def recvfromclient(conn, addr):
+	global LIGHT_ON
+	global THRS
+	while True:
+		data = conn.recv(BUFFER_SIZE)
+		if not data: break
+		data = data.rstrip()
+		print 'Received', data, 'from', addr
+		if data == 'OFF':
+			LIGHT_ON = 'L'
+			conn.send('OK ' + LIGHT_ON + '\n')
+		
+		elif data == 'ON':
+			if getlight() > THRS:		
+				LIGHT_ON = 'H'
+				conn.send('OK ' + LIGHT_ON + '\n')
+			else:
+				conn.send('NOP ' + LIGHT_ON + '\n')
+		else:
+			conn.send('NOK ' + LIGHT_ON + '\n')
+		break
+	conn.close()
+
+while 1:
+	conn, addr = s.accept()
+	recvfromclient(conn,addr)
