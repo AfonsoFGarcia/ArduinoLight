2077f3149b0212d36af21b37683eb802de79c10b
Afonso Garcia <afonsogarcia@icloud.com>
2014-12-02 16:13:40 +0200

    Moved networking to threads to allow faster UI

--- Game/GuessLight.java
+++ Game/GuessLight.java
@@ -5,29 +5,17 @@ public class GuessLight {
     private static String TCP_IP;
     private static Integer TCP_PORT = 5000;
 
-    private static String sendToServer(String message) throws IOException {
-        Socket server = new Socket(TCP_IP, TCP_PORT);
-        PrintWriter out = new PrintWriter(server.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
-
-        out.println(message);
-        String reply =  in.readLine();
-        server.close();
-
-        return reply;
-    }
-
     private static void printMenu() {
-        System.out.println("0 - Exit the game");
-        System.out.println("1 - Guess the number");
-        System.out.println("2 - Reset the number");
+        System.out.println("\n1 - Guess the number");
+        System.out.println("2 - Reset the number\n");
+        System.out.println("0 - Exit the game\n");
         System.out.print("Choose option: ");
     }
 
-    private static void guess() throws IOException {
-        System.out.print("Enter your guess [X X X]: ");
+    private static void guess() {
+        System.out.print("\nEnter your guess [X X X]: ");
         String guess = System.console().readLine();
-        sendToServer("G "+guess);
+        new Thread(new SendMessage("G "+guess)).start();
     }
 
     public static void main(String args[]) {
@@ -39,14 +27,32 @@ public class GuessLight {
         while(true) {
             printMenu();
             String option = System.console().readLine();
+            if (option.equals("0")) {
+                break;
+            } else if (option.equals("1")) {
+                guess();
+            } else if (option.equals("2")) {
+                new Thread(new SendMessage("RES")).start();
+            }
+        }
+    }
+
+    private static class SendMessage implements Runnable {
+        private String message;
+
+        public SendMessage(String message) {
+            this.message = message;
+        }
+
+        public void run() {
             try {
-                if (option.equals("0")) {
-                    break;
-                } else if (option.equals("1")) {
-                    guess();
-                } else if (option.equals("2")) {
-                    sendToServer("RES");
-                }
+                Socket server = new Socket(TCP_IP, TCP_PORT);
+                PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+
+                out.println(message);
+                String reply =  in.readLine();
+                server.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
